88c8b9f6ca6860caa6909b32e88bfbf29209f67e
zeonga1102 <zeonga1102@gmail.com>
2024-03-25 23:31:35 +0900

    Feat: 도서 검색
    * 카카오 책 검색 API를 이용한 도서 검색
    * 투표글 수를 나타내는 필드인 poll_count에 대해서는 추후 수정
    * 기획 시 isbn을 이용하여 비개발 도서를 구분하려는 시도가 있었는데 해당 사항 미반영

--- book/views.py
+++ book/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import requests
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from book.schemas import BookList
+from book.statics import KAKAO_BOOK_SEARCH_URL, KAKAO_BOOK_SEARCH_HEADER, KAKAO_BOOK_SEARCH_SIZE
+
+
+class BookSearchView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        keyword = request.GET.get('keyword')
+        if not keyword:
+            return Response({'message': 'validation error'}, status.HTTP_400_BAD_REQUEST)
+        
+        params = {
+            'query': keyword,
+            'size': KAKAO_BOOK_SEARCH_SIZE
+        }
+        response = requests.get(KAKAO_BOOK_SEARCH_URL, params=params, headers=KAKAO_BOOK_SEARCH_HEADER)
+        if response.status_code != 200:
+            return Response({'message': 'kakao api error'}, status.HTTP_500_INTERNAL_SERVER_ERROR)
+        book_document_list = response.json()['documents']
+
+        book_list = [
+            BookList(
+                poll_count=0,   # 추후 수정
+                **document
+            ).__dict__ for document in book_document_list
+        ]
+
+        return Response({'book_list': book_list}, status.HTTP_200_OK)
