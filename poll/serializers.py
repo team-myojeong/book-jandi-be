a2da4ec2eea27de7a1d735d48437e0246230acb2
zeonga1102 <zeonga1102@gmail.com>
2024-04-02 01:04:39 +0900

    Edit: difficulty_level serializers.IntegerField로 변경

--- poll/serializers.py
+++ poll/serializers.py
@@ -7,11 +7,13 @@ from poll.models import Poll
 class PollSerializer(serializers.ModelSerializer):
     class Meta:
         model = Poll
-        fields = ['level', 'question', 'description', 'user', 'book']
+        fields = ['question', 'description', 'user', 'book', 'difficulty_level']
 
-    def validate(self, data):
-        if not (1 <= data.get('level', 0) <= 3):
+    difficulty_level = serializers.IntegerField(source='level')
+
+    def validate_difficulty_level(self, value):
+        if not (1 <= value <= 3):
             raise ValidationError({'error': 'invalid data'})
 
-        return data
+        return value
     
