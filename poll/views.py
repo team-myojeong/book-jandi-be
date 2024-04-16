8171b2282e596e61e2b2cda9265cbf76b1870088
zeonga1102 <zeonga1102@gmail.com>
2024-04-17 05:01:22 +0900

    Feat: 의견 조회
    * OpinionView permission 변경
      * IsSignupCompleted -> GET: AllowAny, 나머지: IsSignupCompleted

--- poll/views.py
+++ poll/views.py
@@ -4,11 +4,12 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from django.db import transaction
 from django.db.models import Count
+from django.db.models.query_utils import Q
 from django.utils import timezone
 
 from book.models import Book
 from book.serializers import BookSerializer
-from poll.models import Poll, Vote
+from poll.models import Poll, Vote, Opinion
 from poll.serializers import PollSerializer, VoteSerializer, OpinionSerializer, PollSimpleSerializer, PopularPollSerializer
 from bookjandi.permissions import IsSignupComepleted
 
@@ -167,3 +168,86 @@ class PopularPollView(APIView):
         serialized_poll_data = PopularPollSerializer(poll_data, many=True).data
 
         return Response({'poll_list': serialized_poll_data}, status.HTTP_200_OK)
+    
+
+class OpinionView(APIView):
+    def get_permissions(self):
+        """
+        GET Method 요청인 경우 permission AllowAny
+        그 외의 요청은 IsSignupCompleted
+        """
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        
+        return [IsSignupComepleted()]
+
+    def get(self, request):
+        """
+        의견 조회
+        내가 작성한 의견 + 의견 목록
+        """
+        user = request.user
+
+        poll_id = request.GET.get('id')
+        limit = int(request.GET.get('limit', 10))
+        last = request.GET.get('last')
+
+        select_related = ('user', 'user__job', 'user__career', 'poll')
+        condition = Q(poll=poll_id) if last is None else Q(id__lt=last) & Q(poll=poll_id)
+
+        opinion_data = (
+            Opinion.objects
+            .select_related(*select_related)
+            .filter(condition)
+            .order_by('-created_at')[:limit]
+        )
+        serialized_opinion_data = OpinionSerializer(opinion_data, many=True).data
+
+        response = {
+            'my_opinion': None,
+            'opinion_list': serialized_opinion_data
+        }
+
+        if last is None and user.is_authenticated:    # 최초 호출
+            try:
+                opinion_data = Opinion.objects.select_related(*select_related).get(poll=poll_id, user=user)
+            except Opinion.DoesNotExist:
+                return Response(response, status.HTTP_200_OK)
+            
+            response['my_opinion'] = OpinionSerializer(opinion_data).data
+
+        return Response(response, status.HTTP_200_OK)
+
+
+    def post(self, request):
+        """
+        의견 작성
+        내가 작성한 글의 경우 의견 작성 불가능 -> 투표가 본인이 작성한 글은 불가능하므로 확인 안 해도 됨
+        투표한 글만 의견 작성 가능
+        의견은 하나만 작성 가능
+
+        poll 존재 여부
+            -> 투표가 하나라도 됐다면 삭제 불가능하므로 의견을 작성한다는 것은 투표를 했다는 것이고, 그렇다면 poll이 존재
+        """
+        user = request.user
+        request_data = request.data.copy()
+
+        request_data['user'] = user.id
+        request_data['poll'] = request_data['id']
+
+        try:
+            vote = Vote.objects.get(poll=request_data['poll'], user=user)
+        except Vote.DoesNotExist:
+            return Response({'error': 'error'}, status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            opinion = Opinion.objects.get(poll=request_data['poll'], user=user)
+        except Opinion.DoesNotExist:
+            opinion_serialiser = OpinionSerializer(data=request_data)
+            if opinion_serialiser.is_valid():
+                saved_data = opinion_serialiser.save()
+                return Response({'id': saved_data.id}, status.HTTP_200_OK)
+            
+            return Response({'error': 'error'}, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        return Response({'error': 'error'}, status.HTTP_400_BAD_REQUEST)
