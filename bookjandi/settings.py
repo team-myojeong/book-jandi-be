deaac900ec8f2b4305a24dbd4a0cf46bfaffc93b
zeonga1102 <zeonga1102@gmail.com>
2024-05-19 01:51:38 +0900

    Feat: ALLOWED_HOSTS json에서 읽어오게 변경

--- bookjandi/settings.py
+++ bookjandi/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = secrets['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = secrets['ALLOWED_HOSTS']
 
 
 # Application definition
