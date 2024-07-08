382feda54034a1603bd626d02423c0473b40ed02
jeonga.lee <zeonga1102@gmail.com>
2024-07-09 01:56:11 +0900

    Fix: KAKAO_CALLBACK_URI 잘못된 값으로 설정된 오류 수정
    * 프론트와 연결할 때 callback uri는 프론트의 주소여야 함

--- bookjandi/statics.py
+++ bookjandi/statics.py
@@ -4,4 +4,4 @@ BASE_URL = secrets['BASE_URL']
 
 kakao_secrets = secrets['KAKAO']
 KAKAO_REST_API_KEY = kakao_secrets['REST_API_KEY']
-KAKAO_CALLBACK_URI = BASE_URL + '/user/login/'
+KAKAO_CALLBACK_URI = kakao_secrets['CALLBACK_URI']
